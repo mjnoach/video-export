f7dfefebd2052941ffa4b1150dedd62c9a204a1c
mjnoach <a.siennkiewicz@gmail.com>
2024-02-15 04:32:28 +0100

    prettier format

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,9 +1,10 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+
+import * as SliderPrimitive from '@radix-ui/react-slider'
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -12,7 +13,7 @@ const Slider = React.forwardRef<
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      'relative flex w-full touch-none select-none items-center',
       className
     )}
     {...props}
