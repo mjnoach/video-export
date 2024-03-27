d4bd7f523ca9a2ab613901da0193ac18a537ef25
mjnoach <a.siennkiewicz@gmail.com>
2024-03-27 02:10:15 +0100

    update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { VideoLinkForm } from '@/components/video-link-form'
 
 // TODO
+
 // Feature: Multiple slider ranges for exporting a video combined of multiple clips
 // Feature: Add a cropping frame that will only capture the selected area for a given slider range
 
 // Improve: Use SSE instead of raw stream?
 // Improve: Unset video buffering screen after player seek or clip export
-
+// Improve: Update favicon
+// Improve: Configure monorepo with turborepo
 // Improve: Configure secure server with https or/and http2?
+// Improve: Enpodints input validataion
+
+// Fix: Local file upload
+//  if file is local, process in browser ffmpeg rather than on the server?
 
 export default function Home() {
   return (
